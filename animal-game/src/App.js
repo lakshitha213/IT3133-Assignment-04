45c5ecea8cb8466ae07c9df2481e7d303e5cb447
Thilina Lakshitha <108370639+lakshitha213@users.noreply.github.com>
2024-11-22 08:08:40 +0000

    Update App.js

--- animal-game/src/App.js
+++ animal-game/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import React from "react";
+import Game from "./components/Game";
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Game/>
+    
   );
 }
 
